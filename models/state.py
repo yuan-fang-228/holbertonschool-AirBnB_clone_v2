c1442a1997b839b0a422bc081cf5721c5b5109d7
James Honey <jashoney@hotmail.com>
2022-08-08 10:46:09 +1000

    added citylist function to states

--- models/state.py
+++ models/state.py
@@ -3,17 +3,31 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from os import getenv
+import models
+from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
 
     __tablename__ = "states"
-
     name = Column(String(128), nullable=False)
+
 
 if getenv('HBNB_TYPE_STORAGE') == "FileStorage":
     name = ""
+
+    @property
+    def cities(self):
+        """ a getter that returns a list of cities 
+            where city.state_id == states.id
+        """
+        citylist = []
+        cities = models.storage.all(City)
+        for city in cities.values():
+            if city.state_id == self.id:
+                citylist.append(city)
+        return citylist
 
 else:
     """ storage = db """
