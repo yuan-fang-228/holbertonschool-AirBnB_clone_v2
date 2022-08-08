b2d64734fb275ba5822d03e18f602712d4aaba4d
yuan-fang-228 <fy0228@gmail.com>
2022-08-08 04:58:46 -0700

    fixed base_model

--- models/base_model.py
+++ models/base_model.py
@@ -30,8 +30,9 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        dict = self.__dict__.copy()
+        dict.pop('_sa_instance_state', None)
+        return '[{}] ({}) {}'.format(type(self).__name__, self.id, dict)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
@@ -47,8 +48,7 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if "_sa_instance_state" in dictionary:
-            dictionary.pop('_sa_instance_state')
+        dictionary.pop('_sa_instance_state', None)
         return dictionary
 
     def delete(self):
