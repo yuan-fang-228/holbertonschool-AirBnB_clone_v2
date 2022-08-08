857f349a6b73ae04fe57d5dc7f641b0f8b2806b0
Cienna Nguyen <4326@holbertonschool.com>
2022-08-08 13:28:04 +1000

    Update attributes and relationship with other classes

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModelBase
+from sqlalchemy import Column, String, ForeignKey
 
 
 class Amenity(BaseModel):
-    name = ""
+    """Amenities of the place to stay"""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = ""
