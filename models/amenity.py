51987b8034c2e5a0643d205ef586382a5ad7261a
Cienna Nguyen <4326@holbertonschool.com>
2022-08-09 15:04:11 +1000

    Add import

--- models/amenity.py
+++ models/amenity.py
@@ -2,10 +2,14 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
+import OS
 
 
 class Amenity(BaseModel):
     """Amenities of the place to stay"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = ""
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        place_amenities = relationship('Place', secondary=place_amenity)
--- models/place.py
+++ models/place.py
@@ -8,10 +8,10 @@ import models
 
 
 place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60), primary_key=True,
-                             ForeignKey('places.id'), nullable=False),
-                      Column('amenity_id', String(60), primary_key=True,
-                             ForeignKey('amenities.id'), nullable=False),)
+                      Column('place_id', String(60), ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -31,7 +31,7 @@ class Place(BaseModel, Base):
 
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review", backref="place")
-        amenities = relationship("Amenity", secondary="place_amenity",
+        amenities = relationship("Amenity", secondary=place_amenity,
                                  backref="place_amenities", viewonly=False)
     else:
         @property
