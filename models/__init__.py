5d62136fec1cf6bfebf05b812ae447c454502be0
Cienna Nguyen <4326@holbertonschool.com>
2022-08-10 13:34:02 +1000

    Fix import

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """This module instantiates an object of class Storage"""
 import os
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 if os.getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
