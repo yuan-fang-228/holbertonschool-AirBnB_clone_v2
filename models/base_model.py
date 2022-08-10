2305bd7adbc39a15609fd76e7f88f7fc9f2bdff3
yuan-fang-228 <fy0228@gmail.com>
2022-08-09 22:29:21 -0700

    fix pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -24,10 +24,12 @@ class BaseModel:
         else:
             if 'updated_at' in kwargs:
                 kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f')
+                                                         '%Y-%m-%dT%H:%M:%S.%f'
+                                                         )
             if 'created_at' in kwargs:
                 kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f')
+                                                         '%Y-%m-%dT%H:%M:%S.%f'
+                                                         )
             if '__class__' in kwargs:
                 del kwargs['__class__']
             self.__dict__.update(kwargs)
