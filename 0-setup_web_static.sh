4c2e88325776d1b407ec906ecc881767e74e07fe
yuan-fang-228 <98162365+yuan-fang-228@users.noreply.github.com>
2022-08-25 13:18:14 +1000

    Create 0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Bash script to set up web server
+sudo apt-get update -y
+sudo apt-get install -y nginx
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo echo "Holberton School" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
+sudo sed -i '/listen [::]:80 default_server;/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo service nginx restart
