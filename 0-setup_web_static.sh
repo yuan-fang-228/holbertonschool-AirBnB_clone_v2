9ccc0ea6dd7e4fb6a9be381d87f7c5d2b9b14f11
yuan-fang-228 <98162365+yuan-fang-228@users.noreply.github.com>
2022-08-25 14:35:24 +1000

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,8 +6,8 @@ sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 sudo echo "Holberton School" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu /data/
-sudo chgrp -R ubuntu /data/
-sudo sed -i '/listen [::]:80 default_server;/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu /data
+sudo chgrp -R ubuntu /data
+sudo sed -i '/listen 80 default_server;/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 sudo service nginx restart
