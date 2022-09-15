5833e45bf6ab93d3339ac5ceffd1522d4b12ed5b
yuan-fang-228 <fy0228@gmail.com>
2022-09-14 21:10:18 -0700

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""start a flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hnbn():
+    """display Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def show_hnbn():
+    """display HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def show_ctext(text):
+    """display C followed by the value of text"""
+    string = text.replace("_", " ")
+    return f"C {string}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
