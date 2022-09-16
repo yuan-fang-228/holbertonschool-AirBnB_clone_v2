849b0696c40c12fd6c9f0ae73e2ef8cab7f7a9e8
yuan-fang-228 <fy0228@gmail.com>
2022-09-15 18:58:36 -0700

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""start a flask web application for HBNB project"""
+from flask import Flask
+from flask import render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def show_state():
+    """show all the states on the HTML page"""
+    allStates = storage.all(State)
+    return render_template("7-states_list.html", allStates=allStates)
+
+
+@app.teardown_appcontext
+def remove_currentsession():
+    """remove current session"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
